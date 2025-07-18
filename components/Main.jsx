31dc79aa3610905bc98016d928dddd73ac74b9b2
jacquikeating <j.keating@live.ca>
2025-07-17 23:20:04 -0400

    Added Imgflip API call useEffect

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Main() {
     const [memeInfo, setMemeInfo] = useState({
@@ -6,6 +6,14 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+
+    const [memesArr, setMemesArr] = useState([])
+
+    useEffect(() => {
+        fetch(("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setMemesArr(data.data.memes)))
+    }, [])
 
     function handleChange(event) {
         const {value, name} = event.currentTarget;
